f687039d48d78b270cf5a47b87449fbfe17943a9
Dada Ifeoluwani Adedotun <65918863+ifechigo@users.noreply.github.com>
2020-11-27 13:48:01 +0100

    Create problem_one.js

---
+++ problem_one.js
@@ -0,0 +1,20 @@
+//Simple Array Sum
+
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    const arrayLength = ar.length;
+  
+    let arraySum = 0;
+
+    for(let i = 0; i < arrayLength; i++){
+        arraySum += ar[i];
+    }
+    return arraySum
+
+}
